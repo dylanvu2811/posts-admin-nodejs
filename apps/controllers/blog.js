82b444dcd4d6ca987bf9b686503dd0053006d2b4
Duong <duongvu281194@gmail.com>
2019-07-04 16:24:37 +0700

    add theme

--- apps/controllers/blog.js
+++ apps/controllers/blog.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({'message': 'this is blog'});
+    // res.json({'message': 'this is blog'});
+    res.render('blog/index');
 });
 
 module.exports = router;
